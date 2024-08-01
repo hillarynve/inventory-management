171d8e02798dc5e28115cd7fd66cef5de2ebe946
hillarynve <hillaryan@g.ucla.edu>
2024-08-01 12:02:47 -0700

    fixed

--- app/page.js
+++ app/page.js
@@ -1,7 +1,32 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
+import { firestore } from './firebase';
+import {Box, Typography} from '@mui/material'
+import { collection, getDocs, query } from 'firebase/firestore'
 
 export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+  
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc)=> {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+  }
+
   return (
-    <>Hello World</>
-  );
+    <Box>
+      <Typography variant="h1">
+        Inventory Management
+      </Typography>
+    </Box>
+  )
 }
