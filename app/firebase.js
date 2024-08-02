4db8d5681171d175a24195b7c812f692e63d0fdc
hillarynve <157451514+hillarynve@users.noreply.github.com>
2024-08-02 11:27:31 -0700

    hide firebase key

--- app/firebase.js
+++ app/firebase.js
@@ -8,13 +8,6 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-VO2wKG9qtZQTZbRsFrUY3cd4mBKsHjQ",
-  authDomain: "inventory-management-43f8a.firebaseapp.com",
-  projectId: "inventory-management-43f8a",
-  storageBucket: "inventory-management-43f8a.appspot.com",
-  messagingSenderId: "934122644507",
-  appId: "1:934122644507:web:7304426d3c0e704bf384be",
-  measurementId: "G-7RZ7NR8NSN"
 };
 
 // Initialize Firebase
